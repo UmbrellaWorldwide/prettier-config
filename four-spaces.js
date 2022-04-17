398401ba4d3c7ed826443938c48751e2fb25fa5c
Edward Navarro <contacto@edwardnavarro.com>
2022-04-17 01:44:31 -0500

    Add four-spaces config file

---
+++ four-spaces.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: 'none',
+  useTabs: false
+}
