98cb488f27677c7fa9d15c5d7dadc5a06a4c7e4b
Edward Navarro <contacto@edwardnavarro.com>
2022-04-17 01:44:19 -0500

    Add base prettier config

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
